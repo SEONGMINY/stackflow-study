7c3287a06aacb1bd614e580e6b1deb888c343924
SEONGMINY <leesm2075@naver.com>
2024-11-23 18:32:52 +0900

    feat: stackflow push

--- src/activity/MainActivity.jsx
+++ src/activity/MainActivity.jsx
@@ -1,9 +1,17 @@
 import { AppScreen } from "@stackflow/plugin-basic-ui";
+import { useFlow } from "@stackflow/react/future";
 
 const MainActivity = () => {
+  const { push } = useFlow();
+
+  const onClickPush = () => {
+    push("ArticleActivity", { title: "아티클 화면 입니까?" });
+  };
+
   return (
     <AppScreen appBar={{ title: "메인 화면 타이틀" }}>
       <div>메인 화면 입니다.</div>
+      <button onClick={onClickPush}>Article 페이지로 이동</button>
     </AppScreen>
   );
 };
--- src/stackflow/index.js
+++ src/stackflow/index.js
@@ -3,6 +3,7 @@ import { stackflow } from "@stackflow/react";
 import { basicRendererPlugin } from "@stackflow/plugin-renderer-basic";
 import { basicUIPlugin } from "@stackflow/plugin-basic-ui";
 import MainActivity from "../activity/MainActivity";
+import ArticleActivity from "../activity/ArticleActivity";
 
 export const { Stack, useFlow } = stackflow({
   transitionDuration: 300,
@@ -15,6 +16,7 @@ export const { Stack, useFlow } = stackflow({
   ],
   activities: {
     MainActivity,
+    ArticleActivity,
   },
   initialActivity: () => "MainActivity",
 });
