8a1eba7d255f6300eab2d88916f25f86cc3ff4cb
SEONGMINY <leesm2075@naver.com>
2024-11-23 18:35:47 +0900

    feat: stackflow pop, replace

--- src/activity/ArticleActivity.jsx
+++ src/activity/ArticleActivity.jsx
@@ -1,10 +1,23 @@
 import { AppScreen } from "@stackflow/plugin-basic-ui";
+import { useFlow } from "../stackflow";
 
 const ArticleActivity = ({ params: { title } }) => {
+  const { replace, pop } = useFlow();
+
+  const onClickPop = () => {
+    pop();
+  };
+
+  const onClickReplace = () => {
+    replace("ArticleActivity", { title: "Replace된 아티클" });
+  };
+
   return (
     <AppScreen appBar={{ title: "Article" }}>
       <div>
         <h1>{title}</h1>
+        <button onClick={onClickPop}>뒤로가기</button>
+        <button onClick={onClickReplace}>Replace</button>
       </div>
     </AppScreen>
   );
