ca38799e296f7259efc9b85160e73ae3286f1758
SEONGMINY <leesm2075@naver.com>
2024-11-23 18:18:38 +0900

    feat: stackflow render

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Stack } from "./stackflow";
 
 function App() {
--- src/stackflow/index.js
+++ src/stackflow/index.js
@@ -1,7 +1,15 @@
+import "@stackflow/plugin-basic-ui/index.css";
 import { stackflow } from "@stackflow/react";
+import { basicRendererPlugin } from "@stackflow/plugin-renderer-basic";
+import { basicUIPlugin } from "@stackflow/plugin-basic-ui";
 
 export const { Stack, useFlow } = stackflow({
   transitionDuration: 300,
   activities: {},
-  plugins: [],
+  plugins: [
+    basicRendererPlugin(),
+    basicUIPlugin({
+      theme: "cupertino",
+    }),
+  ],
 });
